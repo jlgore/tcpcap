8600aad77a5de717656ec8374156f0e7636ea48b
Jared Gore <jg@gore.cc>
2022-04-21 11:48:07 +0000

    adding prometheus code

--- code/main.go
+++ code/main.go
@@ -5,9 +5,14 @@ import (
 	"log"
 	"time"
 
+	"net/http"
+
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type logWriter struct {
@@ -30,13 +35,50 @@ const (
 	defaultSnapLen = 262144
 )
 
+var (
+	connsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "tcpcap_processed_conns_total",
+		Help: "The total number of processed connections",
+	})
+)
+
 // set your interface here
 
-var iface string = "enp0s8"
+var iface string = "eth0"
 
 // var bpfsyn string = "tcp[13] = 3"
 
-func main() {
+func inTimeSpan(start, end, check time.Time) bool {
+	if start.Before(end) {
+		return !check.Before(start) && !check.After(end)
+	}
+	if start.Equal(end) {
+		return check.Equal(start)
+	}
+	return !start.After(check) || !end.Before(check)
+}
+
+func stamper(timestamps []time.Time) {
+	end := time.Now()
+	start := end.Add(-60 * time.Second)
+	for _, t := range timestamps {
+		inTimeSpan(start, end, t)
+	}
+}
+
+func capMe() {
+	/*
+		here I am opening a capture on a selected interface and filtering it with a BPF filter
+		the bpf query is just a variable that we will default to tcp[13] = 3 to capture syn connections
+		from the chosen interface
+	*/
+
+	listener, err := pcap.OpenLive(iface, defaultSnapLen, true,
+		pcap.BlockForever)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
 
 	// enforcing a specific time-date format for the log output
 
@@ -46,30 +88,18 @@ func main() {
 	// make the struct for the connections
 	c := Connections{}
 
-	/*
-		here I am opening a capture on a selected interface and filtering it with a BPF filter
-		the bpf query is just a variable that we will default to tcp[13] = 3 to capture syn connections
-		from the chosen interface
-	*/
-
-	handle, err := pcap.OpenLive(iface, defaultSnapLen, true,
-		pcap.BlockForever)
-	if err != nil {
-		panic(err)
-	}
-	defer handle.Close()
-
 	// tcp and tcp[tcpflags] == tcp-syn or
 	// TODO: confirm correct set of filters for BPF for syn packets.
 
-	if err := handle.SetBPFFilter("tcp"); err != nil {
+	if err := listener.SetBPFFilter("tcp and tcp[tcpflags] == tcp-syn"); err != nil {
 		panic(err)
 	}
 
 	log.Println("Listening on", iface, "...")
 
 	packets := gopacket.NewPacketSource(
-		handle, handle.LinkType()).Packets()
+		listener, listener.LinkType()).Packets()
+
 	for pkt := range packets {
 
 		packet := gopacket.NewPacket(pkt.Data(), layers.LayerTypeEthernet, gopacket.Default)
@@ -80,20 +110,36 @@ func main() {
 		// TODO switch to switch??
 
 		if c.Address == addr {
-			log.Printf("Previous Count: %d, Previous Connect: %s", c.Count, c.Timestamps)
+
+			stamper(c.Timestamps)
+
+			log.Printf("Previous Connection Found")
+			c.Count++
+			connsProcessed.Inc()
+			log.Printf("Repeat Connection: %s has connected before %d times.\n", addr, c.Count)
+
 			if c.Count >= 3 {
 				log.Printf("TO DO: Block")
 			}
-			c.Count++
-			log.Printf("Repeat Connection: %s has connected before %d times.\n", addr, c.Count)
 		} else {
 			c.Address = addr
 			c.Count = 1
 			c.Timestamps = append(c.Timestamps, time.Now())
+			connsProcessed.Inc()
 		}
 
 		log.Printf("New Connection: %s:%s -> %s:%s\n", l2.SrcIP, tcp.SrcPort, l2.DstIP, tcp.DstPort)
 
 	}
+}
+
+func main() {
+
+	go capMe()
+
+	// prom setup
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 
 }
